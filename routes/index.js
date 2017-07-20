ed192c5305dd0799ca2eeba3215c249c001ecd23
frantiq90 <oscar.kosela@gmail.com>
2017-07-20 13:22:21 +0000

    user has to be logged before checkout

--- routes/index.js
+++ routes/index.js
@@ -44,7 +44,7 @@ router.get('/shopping-cart', function(req, res, next) {
     res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
-router.get('/checkout', function(req, res, next) {
+router.get('/checkout', isLoggedIn, function(req, res, next) {
     if (!req.session.cart) {
         return res.redirect('/shopping-cart');
     }
@@ -53,7 +53,7 @@ router.get('/checkout', function(req, res, next) {
     res.render('shop/checkout', {total: cart.totalPrice, errMsg: errMsg, noError: !errMsg });
 });
 
-router.post('/checkout', function(req, res, next) {
+router.post('/checkout', isLoggedIn, function(req, res, next) {
     if (!req.session.cart) {
         return res.redirect('/shopping-cart');
     }
@@ -92,5 +92,13 @@ router.post('/checkout', function(req, res, next) {
 
     });
 });
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    req.session.oldUrl = req.url;
+    res.redirect('/user/signin');
+}
 
 module.exports = router;
